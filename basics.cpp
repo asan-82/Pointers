eda9bf983440f00e8d3e659274176701345d776e
AArusshi82 <143922485+AArusshi82@users.noreply.github.com>
2025-06-14 11:54:22 +0530

    pointer variable declared

--- basics.cpp
+++ basics.cpp
@@ -1,3 +1,8 @@
+// address is a hexadecimal value
+// Pointers are special variables used to store addresses
+// The type of a pointer variable should be same as the type of the value stored at the memory location it points to  
+// even the pointer variable would have its own address
+
 #include<iostream>
 using namespace std;
 
@@ -5,4 +10,8 @@ int main()
 {
     int a=5;
     cout<<&a; //gives the address of a
+
+    int *ptr1; //using this u can store address of a variable storing integer value/ int variable
+
+    float *ptr2; //using this u can store address of a variable storing float value/ float variable
 }
